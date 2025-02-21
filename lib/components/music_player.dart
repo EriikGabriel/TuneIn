24810c7402543f81df4253d7d8e65d47f5f91f95
EriikGabriel <erikgabriel.lins@hotmail.com>
2025-02-21 14:54:16 -0300

    feat: organize

--- lib/components/music_player.dart
+++ lib/components/music_player.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class MusicPlayer extends StatefulWidget {
-  const MusicPlayer({Key? key}) : super(key: key);
+  const MusicPlayer({super.key});
 
   @override
   State<MusicPlayer> createState() => _MusicPlayerState();
